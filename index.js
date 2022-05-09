c276799e1c737d4e159a3b1c6700a4a2153b24b1
willyhp123 <limwilly0@gmail.com>
2022-05-09 11:29:45 +0700

    Upload

--- index.js
+++ index.js
@@ -23,4 +23,6 @@ app.use((req, res, next) => {
   });
 });
 
-app.listen(5000, console.log("Server listening on port"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
