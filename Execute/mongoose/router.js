407cb45966e4e4e449d3f447149cc4adefa29840
willyhp123 <limwilly0@gmail.com>
2022-05-09 11:31:58 +0700

    Upload

--- Execute/mongoose/router.js
+++ Execute/mongoose/router.js
@@ -1,16 +1,14 @@
-const router = require('express').Router();
-const multer = require('multer');
-const upload = multer({ desk: 'uploads' });
-const ProductController = require('./controller');
+const router = require("express").Router();
+const multer = require("multer");
+const upload = multer({ desk: "uploads" });
+const ProductController = require("./controller");
 
+router.get("/", ProductController.index);
+router.get("/productz/", ProductController.index);
+router.get("/productz/:search", ProductController.search);
+router.post("/productz", upload.single("image"), ProductController.store);
+router.get("/productz/:id", ProductController.view);
+router.patch("/productz/:id", upload.single("image"), ProductController.update);
+router.delete("/productz/:id", ProductController.destroy);
 
-router.get('/productz/', ProductController.index);
-router.get('/productz/:search', ProductController.search);
-router.post('/productz', upload.single('image'), ProductController.store);
-router.get('/productz/:id', ProductController.view);
-router.patch('/productz/:id', upload.single('image'), ProductController.update);
-router.delete('/productz/:id', ProductController.destroy);
-
-  
-  
 module.exports = router;
