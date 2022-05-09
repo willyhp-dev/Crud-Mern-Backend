2388c9f71c79e1c69224a1f9bfb5079a15de0498
willyhp123 <limwilly0@gmail.com>
2022-05-09 12:58:01 +0700

    Upload

--- Execute/mongoose/router.js
+++ Execute/mongoose/router.js
@@ -5,10 +5,10 @@ const ProductController = require("./controller");
 
 router.get("/", ProductController.index);
 router.get("/productz/", ProductController.index);
-router.get("/productz/search/:search", ProductController.search);
+router.get("/productz/:search", ProductController.search);
 router.post("/productz", upload.single("image"), ProductController.store);
-router.get("/productz/:id", ProductController.view);
-router.patch("/productz/:id", upload.single("image"), ProductController.update);
-router.delete("/productz/:id", ProductController.destroy);
+router.get("/detail/:id", ProductController.view);
+router.patch("/update/:id", upload.single("image"), ProductController.update);
+router.delete("/delete/:id", ProductController.destroy);
 
 module.exports = router;
